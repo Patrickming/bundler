58bd4f00c4db47259930373f674f362229ea20c6
Patrickming <patrickmingx@gmail.com>
2024-06-26 22:10:16 +0800

    删掉了数据库的读写

--- config/Config.go
+++ config/Config.go
@@ -1,12 +1,7 @@
 package config
 
 import (
-	"context"
-	"os"
-
 	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // LoadEnv 加载 .env 文件中的环境变量
@@ -16,20 +11,20 @@ func LoadEnv() {
 	}
 }
 
-// GetMongoClient 创建并返回一个 MongoDB 客户端
-func GetMongoClient() (*mongo.Client, error) {
-	mongoURI := os.Getenv("MONGO_URI")
+// // GetMongoClient 创建并返回一个 MongoDB 客户端
+// func GetMongoClient() (*mongo.Client, error) {
+// 	mongoURI := os.Getenv("MONGO_URI")
 
-	clientOptions := options.Client().ApplyURI(mongoURI)
-	client, err := mongo.Connect(context.TODO(), clientOptions)
-	if err != nil {
-		return nil, err
-	}
+// 	clientOptions := options.Client().ApplyURI(mongoURI)
+// 	client, err := mongo.Connect(context.TODO(), clientOptions)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	err = client.Ping(context.TODO(), nil)
-	if err != nil {
-		return nil, err
-	}
+// 	err = client.Ping(context.TODO(), nil)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	return client, nil
-}
+// 	return client, nil
+// }
